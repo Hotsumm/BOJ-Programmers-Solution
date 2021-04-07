f40c2184d74d28d68058d8407a9737e29db23ee8
SungYeolha <tjddufgk@gmail.com>
2021-04-07 20:12:44 +0900

    📱2020Kakao키패드누르기

---
+++ Programmers/KaKao/2020키패드누르기.py
@@ -0,0 +1,31 @@
+def solution(numbers, hand):
+    answer = ''
+    left = 10
+    right = 12
+    for i in numbers:
+        if i == 1 or i == 4 or i == 7:
+            answer += 'L'
+            left = i
+        elif i == 3 or i == 6 or i == 9:
+            answer += 'R'
+            right = i 
+        else :
+            if i == 0:
+                i = 11
+            left_calc = abs(i-left)//3 + abs(i-left)%3
+            right_calc = abs(i-right)//3 + abs(i-right)%3
+            if  left_calc < right_calc:
+                answer += 'L'
+                left = i
+            elif left_calc > right_calc:
+                answer += 'R'
+                right = i
+            else :
+                if hand =="right":
+                    answer += 'R'
+                    right = i 
+                else :
+                    answer += 'L'
+                    left = i
+    return answer
+
