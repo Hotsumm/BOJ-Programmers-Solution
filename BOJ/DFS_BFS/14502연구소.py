cb1e5cfbc1a97d147adbaf5378ccbb3901b05941
SungYeolha <tjddufgk@gmail.com>
2021-04-28 03:07:29 +0900

    🔬연구소

--- BOJ/DFS_BFS/14502연구소.py
+++ BOJ/DFS_BFS/14502연구소.py
@@ -1,31 +1,51 @@
 from collections import deque
-dx = [1,-1,0,0]
+from itertools import combinations
+import copy
+import sys
+dx = [-1,1,0,0]
 dy = [0,0,1,-1]
 
+
 def bfs():
-    while queue:
-        a,b = queue.popleft()
+    cnt = 0
+    deq = deque()
+    for i,j in virus:
+        deq.append([i,j])
+    while deq:
+        x,y = deq.popleft()
         for k in range(4):
-            x = a + dx[k]
-            y = b + dy[k]
-            if 0<=x<n and 0<=y<m and lab[x][y] == 0 :
-                lab[x][y] = 2
-                queue.append([x,y])
-
-n, m = map(int,input().split())
-queue = deque()
+            nx = x+dx[k]
+            ny = y+dy[k]
+            if 0<=nx<n and 0<=ny<m:
+                if graph[nx][ny]==0 and copy_graph[nx][ny] == 0:    
+                    copy_graph[nx][ny] = 2 
+                    deq.append([nx,ny])
+                    cnt +=1
+    return cnt
+    
+    
+n,m = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().split())) for i in range(n)]
 empty = []
-lab = []
-virus_list = []
-
-for i in range(n):
-    lab.append(list(map(int,input().split())))
+virus = []
+safety = 0
 
 for i in range(n):
     for j in range(m):
-        if lab[i][j] == 2:
-            queue.append([i,j])
-        elif lab[i][j] == 0:
-            empty.append([i,j])       
-bfs()
+        if graph[i][j] == 0:
+            empty.append((i,j))
+        elif graph[i][j] == 2:
+            virus.append([i,j])
+combi = list(combinations(empty,3))
 
+for c in combi:
+    cnt = 0
+    copy_graph = copy.deepcopy(graph)
+    for a,b in c :
+        copy_graph[a][b] = 1
+    bfs()
+    for r in copy_graph:
+        cnt += r.count(0)
+    safety = max(safety,cnt)
+    
+print(safety)
