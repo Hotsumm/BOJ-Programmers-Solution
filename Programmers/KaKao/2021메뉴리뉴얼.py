886e53e5cabe60b40188c5d91fe63f5924f292ae
SungYeolha <tjddufgk@gmail.com>
2021-05-21 18:41:08 +0900

    ✨2018Kakao메뉴리뉴얼

--- Programmers/KaKao/2021메뉴리뉴얼.py
+++ Programmers/KaKao/2021메뉴리뉴얼.py
@@ -1,13 +1,20 @@
 from itertools import combinations
+from collections import Counter
 
 def solution(orders, course):
     answer = []
-    for i in course:
+    
+    for c in course:
         menu = []
+        max_v = 0
         for order in orders:
-            for combi in combinations(order,i):
-                menu.append(''.join(sorted(combi)))
-        print(menu)
+            for combi in combinations(sorted(order),c):
+                menu.append(''.join(combi))
+        combi_menu = Counter(menu)
+        for k,v in combi_menu.items():
+            if v>=2 and v == max(combi_menu.values()):
+                answer.append(k)
+                
+    answer.sort()
+
     return answer
-
-
